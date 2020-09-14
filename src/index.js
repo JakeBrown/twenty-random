8d846d649a073e49850a29ecd9b2811498f77764
Jake Brown <jake@eyespacelenses.com>
2020-09-14 17:37:55 +0930

    Set up release

--- src/index.js
+++ src/index.js
@@ -6,12 +6,18 @@ import App from './App.js'
 import * as Sentry from '@sentry/react'
 import { Integrations } from '@sentry/tracing'
 
-Sentry.init({
+let commit = process.env.COMMIT_REF
+let config = {
   dsn:
     'https://faad86b5e28f4f6592dcfadff67fcdb2@o447635.ingest.sentry.io/5427710',
   integrations: [new Integrations.BrowserTracing()],
   tracesSampleRate: 1.0,
-})
+  release: commit ? commit : "LOCAL"
+}
+
+Sentry.init(config)
+console.log(commit)
+console.log(config)
 
 ReactDOM.render(
   <React.StrictMode>
